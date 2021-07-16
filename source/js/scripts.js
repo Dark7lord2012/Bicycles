e26e41acecee3f1cee29457645dfc664679bc95d
Dark7lord <budda185@mail.ru>
2021-07-16 16:29:46 +0500

    Написал скрипты для открытия меню шапки

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -1,4 +1,10 @@
 'use strict';
 
 const header = document.querySelector('.header');
-console.log(header);
+const headerToggle = document.querySelector('.header__toggle');
+
+header.classList.remove('header--no-js');
+
+headerToggle.addEventListener('click', () => {
+  header.classList.toggle('header--opened');
+});
