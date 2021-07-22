d610440b366026c46e8ada0b02542d44a386210d
Dark7lord <budda185@mail.ru>
2021-07-22 15:23:20 +0500

    Внес исправления после ревью (подключил шрифты, добавил комментарии, какие-то стили упростил)

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -1,10 +1,13 @@
 'use strict';
 
-const header = document.querySelector('.header');
-const headerToggle = document.querySelector('.header__toggle');
+(() => {
+  // Открытие / закрытие мобильной шапки
+  const header = document.querySelector('.header');
+  const headerToggle = document.querySelector('.header__toggle');
 
-header.classList.remove('header--no-js');
+  header.classList.remove('header--no-js');
 
-headerToggle.addEventListener('click', () => {
-  header.classList.toggle('header--opened');
+  headerToggle.addEventListener('click', () => {
+    header.classList.toggle('header--opened');
+  });
 });
